7f0698f20d56ecd1c994c76a651070ef434bbf98
jenny obryant <jennyobryant@protonmail.com>
2019-08-31 12:09:32 -0600

    styling draft

--- starwars/src/components/Cards.js
+++ starwars/src/components/Cards.js
@@ -1,10 +1,10 @@
 import React from 'react'; //second step, import react
-//import "./cards.scss"; 
+import {Card } from 'semantic-ui-react'; 
 
 function Cards(props){//third step, define the components
     console.log(props); 
     return ( //5th step, return some js and hoop,add props to component
-        <div className ="card"> 
+        <Card className ="card"> 
            <h2>{props.data.name}</h2>
            <li>Height: {props.data.height}</li>
            <li>Mass: {props.data.mass}</li>
@@ -13,7 +13,7 @@ function Cards(props){//third step, define the components
            <li>Eye Color: {props.data.eye_color}</li>
            <li>Birth Year: {props.data.birth_year}</li>
            <li>Gender: {props.data.gender}</li>
-        </div>
+        </Card>
     ); 
 }
 
